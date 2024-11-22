00f87785be81af8d0281567dd00bb42d389ecdef
Camille Lebrun <c_lebrun3@hetic.eu>
2024-11-22 18:59:36 +0100

    fix

--- models/gold/ratio_sales_win_sector.sql
+++ models/gold/ratio_sales_win_sector.sql
@@ -3,4 +3,4 @@ select
     sector,
     round(countif(deal_stage = 'Won') / count(*), 2) as count
 from {{ ref('sales_team_perf') }}
-group by product
+group by sector
--- models/gold/top_product.sql
+++ models/gold/top_product.sql
@@ -1,14 +1,8 @@
-select
-    product,
-    'N/A' as avg_sales_margin,
-    count(*) as deals_won
-from {{ ref('sales_team_perf') }}
-where deal_stage = 'Won'
-group by product
-union all
+
 select
     product,
     avg(sales_margin) as avg_sales_margin,
     count(*) as deals_won
 from {{ ref('sales_team_perf') }}
 where deal_stage = 'Won'
+group by product
